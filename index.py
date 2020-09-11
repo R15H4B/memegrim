8741a80333a9c5afda578a2b21e70eb48388d394
R15H4B <48987167+R15H4B@users.noreply.github.com>
2020-09-11 12:49:08 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,24 @@
+import os
+
+from utils import default
+from utils.data import Bot, HelpFormat
+
+config = default.get("config.json")
+print("Logging in...")
+
+bot = Bot(
+    command_prefix=os.environ['prefix'],
+    prefix=os.environ['prefix'],
+    command_attrs=dict(hidden=True),
+    help_command=HelpFormat()
+)
+
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
+
+try:
+    bot.run(os.environ['token'])
+except Exception as e:
+    print(f'Error when logging in: {e}')
