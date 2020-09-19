09c478a92c7cb5ffb13ab952f1dbeec395f1b824
R15H4B <R15H4B@users.noreply.github.com>
2020-09-19 11:23:00 +0530

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -78,7 +78,7 @@ class Fun_Commands(commands.Cog):
     async def greet(self, ctx, *, user: discord.Member = None):
         """ Greetings from bot to somone """
         user = user or ctx.author
-        await ctx.send("hello **{user.name}**" or "hi **{user.name}**")
+        await ctx.send(f"hello **{user.name}**" or f"hi **{user.name}**")
 
     @commands.command()
     @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
