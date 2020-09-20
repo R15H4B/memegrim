106a15f62b8724d7882a3170a32f20704dd219a0
R15H4B <R15H4B@users.noreply.github.com>
2020-09-20 21:10:33 +0530

    Update weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -19,18 +19,22 @@ class Weather(commands.Cog, name='Weather'):
         if forecast:
             return rget(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&APPID={environ['WEATHER_TOKEN']}").json()
         data  = rget(f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={environ['WEATHER_TOKEN']}").json()
-        cleared_data = {
-            'Error': data['message'],
-            'City': data['name'],
-            'Weather forecast': f"{data['weather'][0]['main']} - {data['weather'][0]['description']}",
-            'Temperature': f"{data['main']['temp']}°C",
-            'Humidity': f"{data['main']['humidity']}%",
-            'Pressure': f"{data['main']['pressure']} Pa",
-            'Clouds': f"{data['clouds']['all']}%",
-            'Wind': f"{data['wind']['speed']} km/h",
-            'Sunset': (datetime.utcfromtimestamp(data['sys']['sunset']) + timedelta(hours=2)).strftime('%H:%M:%S'),
-            'Sunrise': (datetime.utcfromtimestamp(data['sys']['sunrise']) + timedelta(hours=2)).strftime('%H:%M:%S'),
-        }
+        if data['cod'] == '200':
+            cleared_data = {
+                'City': data['name'],
+                'Weather forecast': f"{data['weather'][0]['main']} - {data['weather'][0]['description']}",
+                'Temperature': f"{data['main']['temp']}°C",
+                'Humidity': f"{data['main']['humidity']}%",
+                'Pressure': f"{data['main']['pressure']} Pa",
+                'Clouds': f"{data['clouds']['all']}%",
+                'Wind': f"{data['wind']['speed']} km/h",
+                'Sunset': (datetime.utcfromtimestamp(data['sys']['sunset']) + timedelta(hours=2)).strftime('%H:%M:%S'),
+                'Sunrise': (datetime.utcfromtimestamp(data['sys']['sunrise']) + timedelta(hours=2)).strftime('%H:%M:%S'),
+            }
+        else:
+            cleared_data = {
+                'Error': data['message'],
+            }
         return cleared_data
 
     @commands.command(brief='weather [City]', description="Get weather forecast of a city")
